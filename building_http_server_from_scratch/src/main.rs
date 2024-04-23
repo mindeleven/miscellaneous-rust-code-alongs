d9de87f630bdba38aa1a2b67924a5fd831d16232
mindeleven <mindeleven@posteo.de>
2024-04-23 13:06:41 +0200

    Storing request methods in an Enum

--- building_http_server_from_scratch/src/main.rs
+++ building_http_server_from_scratch/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code, unused_variables)]
 /// code along with the Udemy course Learn Rust by Building Real Applications
 /// by Lyubomir Gavadinov, https://www.udemy.com/course/rust-fundamentals/
 /// Section 4: Building a HTTP server from scratch
@@ -24,7 +25,36 @@ impl Server {
     }
 }
 
+// request we want to code
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
 fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+    
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
 }
