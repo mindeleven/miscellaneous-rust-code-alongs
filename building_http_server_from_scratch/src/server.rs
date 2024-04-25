2b14ed2117442e3880b8bacaff8fea65bf8b32ef
mindeleven <mindeleven@posteo.de>
2024-04-25 13:53:44 +0200

    Converting buffer and matching the Request::try_from result

--- building_http_server_from_scratch/src/server.rs
+++ building_http_server_from_scratch/src/server.rs
@@ -4,7 +4,15 @@
 
 // TcpListener -> a TCP socket server, listening for connections
 // Struct std::net::TcpListener
-use std::{io::Read, net::{TcpListener, TcpStream}};
+use std::{
+    io::Read, 
+    net::{
+        TcpListener, 
+        TcpStream
+    }
+};
+use crate::http::Request;
+use std::convert::TryFrom;
 
 // TcpStream -> a TCP stream between a local and a remote socket
 // Struct std::net::TcpStream
@@ -43,6 +51,18 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            // buffer needs to be converted from a refernce to a byte slice
+                            // one way to do it -> &buffer as &[u8]
+                            // shorter way: 
+                            // [..] creates a byte slice the omits bounds and contains entire array
+                            // returns result so we've to match
+                            match Request::try_from(&buffer[..]) {
+                                // Ok() wraps request
+                                Ok(request) => {},
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                },
+                            }
                         },
                         Err(e) => println!("Failed to read from connection: {:?}", e),
                     }
