4c34598fb5d8674f4629639659c9b298124b8410
mindeleven <mindeleven@posteo.de>
2024-04-29 10:28:22 +0200

    Using an enum to store the values of the query string

--- building_http_server_from_scratch/src/http/query_string.rs
+++ building_http_server_from_scratch/src/http/query_string.rs
@@ -2,6 +2,19 @@
 use std::collections::HashMap;
 
 struct QueryString<'a> {
-    data: HashMap<&'a str, &'a str>,
+    data: HashMap<&'a str, Value<'a>>,
 }
 
+// for some values we need a string for others an array
+// we're using a vector to store multiple values because 
+// we need a heap allocated array that can have a varying number of values
+pub enum Value<'a> {
+    Single(&'a str),
+    Multiple(Vec<&'a str>),
+}
+
+impl<'a> QueryString<'a> {
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        self.data.get(key)
+    }
+}
