724062eddc87782ceafc8dafd3b834e1549b50e7
mindeleven <mindeleven@posteo.de>
2024-04-29 12:13:39 +0200

    Splitting up the string and pushing it into the QueryString hash map

--- building_http_server_from_scratch/src/http/query_string.rs
+++ building_http_server_from_scratch/src/http/query_string.rs
@@ -18,3 +18,49 @@ impl<'a> QueryString<'a> {
         self.data.get(key)
     }
 }
+
+// converting incoming string from request into a QueryString struct
+// string structure example:
+// a=1&b=2&c&d=&e===&d=7&d=abc
+impl<'a> From<&'a str> for QueryString<'a> {
+    fn from(s: &'a str) -> Self {
+        let mut data = HashMap::new();
+
+        // splitting up the string
+        for sub_str in s.split('&') {
+            let mut key = sub_str;
+            let mut val = "";
+            if let Some(i) = sub_str.find('=') {
+                key = &sub_str[..i];
+                val = &sub_str[i + 1..];
+
+            }
+
+            // is the key already in the hashmap?
+            data.entry(key)
+                // if yes 
+                // and_modify accepts hashmap itself as parameter
+                // it provides in-place mutable access to an occupied entry 
+                // before any potential inserts into the map
+                .and_modify(|existing: &mut Value| {
+                    match existing {
+                        Value::Single(prev_val) => {
+                            // existing is a pointer that gets derefferenced with * so
+                            // it can be overwritten with a value that points somewhere else
+                            *existing = Value::Multiple(vec![prev_val, val]);
+                        },
+                        Value::Multiple(vec) => { vec.push(val)}
+                    }
+                })
+                // if no, insert with or_insert()
+                .or_insert(Value::Single(val));
+                
+
+        }
+
+        QueryString { data };
+
+        unimplemented!()
+    }
+
+}
