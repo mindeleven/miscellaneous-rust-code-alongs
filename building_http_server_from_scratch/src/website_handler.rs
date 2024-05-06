e1a01cd3650bb70e3dd44d8fcce0d13075991040
mindeleven <mindeleven@posteo.de>
2024-05-06 15:39:46 +0200

    Adding routing for incoming requests

--- building_http_server_from_scratch/src/website_handler.rs
+++ building_http_server_from_scratch/src/website_handler.rs
@@ -1,14 +1,47 @@
 use super::server::Handler;
 use super::http::{
-    Request, Response, StatusCode
+    Method, Request, Response, StatusCode
 };
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
+        /* 
         Response::new(
             StatusCode::OK,
             Some("<h1>It's alive!!!!!!</h1><h2>handle request via WebsiteHandler.</h2>".to_string())
         )
+        */
+        // mathing on the incoming method
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => {
+                    Response::new(
+                        StatusCode::OK,
+                        Some("<h1>It's alive!!!!!!</h1><h2>handle request via match request.method().</h2>".to_string())
+                    )
+                },
+                "/hello" => {
+                    Response::new(
+                        StatusCode::OK,
+                        Some("<h1>A simple hello, nothing special</h2>".to_string())
+                    )
+                },
+                _ => {
+                    Response::new(
+                        StatusCode::NotFound,
+                        None
+                    )
+                }
+            },
+            _ => {
+                Response::new(
+                    StatusCode::NotFound,
+                    None
+                )
+            }
+        }
     }
 }
+
+// implementing routing logic in the handler
