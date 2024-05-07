8dcac88b40e7e8fcd6eea8f9d63436d80417b504
mindeleven <mindeleven@posteo.de>
2024-05-07 14:44:17 +0200

    Added default path for starting the server

--- building_http_server_from_scratch/src/main.rs
+++ building_http_server_from_scratch/src/main.rs
@@ -20,10 +20,17 @@ use http::Request;
 use http::Method;
 use website_handler::WebsiteHandler;
 
-fn main() {    
+fn main() {
+    // reading cargo environment variable at compile time
+    // the env macro provides variables that are set at compile time
+    // CARGO_MANIFEST_DIR is the directory where the Cargo.toml file is located
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    
     // setting the public path before running the program with
     // PUBLIC_PATH=$(pwd)/public cargo run
-    let public_path = env::var("PUBLIC_PATH").unwrap();
+    // unwrap_or will path default path in case of error
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public_path: {}", public_path);
     
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run(WebsiteHandler::new(public_path));
