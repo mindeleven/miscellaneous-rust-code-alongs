dff1c71a19b1ee7b666ef3dfeddc4b34b027551f
mindeleven <mindeleven@posteo.de>
2024-05-10 13:40:06 +0200

    Writing out hash() and hex_to_string() functionality

--- building_local_blockchain/src/blockchain.rs
+++ building_local_blockchain/src/blockchain.rs
@@ -132,10 +132,6 @@ impl Chain {
         true
     }
 
-    fn hash(blockheader: &Blockheader) -> String {
-        unimplemented!()
-    }
-
     fn get_merkle(curr_transaction: Vec<Transaction>) -> String {
         let mut merkle = Vec::new();
         for t in curr_transaction {
@@ -144,7 +140,7 @@ impl Chain {
         }
 
         if merkle.len() % 2 == 1 {
-            let last = merkle.last().clone().unwrap();
+            let last = merkle.last().cloned().unwrap();
             merkle.push(last);
         }
 
@@ -178,6 +174,24 @@ impl Chain {
                 }
             }
         }
+    }
+
+    fn hash<T: serde::Serialize>(item: &T) -> String {
+        let input = serde_json::to_string(&item).unwrap();
+        let mut hasher = Sha256::default();
+        hasher.input(input.as_bytes());
+        let res = hasher.result();
+        let vec_res = res.to_vec();
+
+        Chain::hex_to_string(vec_res.as_slice())
+    }
+
+    fn hex_to_string(vec_res: &[u8]) -> String {
+        let mut s = String::new();
+        for b in vec_res {
+            write!(&mut s, "{:x}", b).expect("unable to write");
+        }
+        s
     }
 
 }
