9483cdb20b239db928d5b4befa616ffd97cbab9d
mindeleven <mindeleven@posteo.de>
2024-05-15 14:53:21 +0200

    Basic structure to call Rust functionality from JavaScript

--- rusty-wasm/src/lib.rs
+++ rusty-wasm/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// wasm-pack uses wasm-bindgen to provide a bridge between the types of JavaScript and Rust
+// it allows JavaScript to call a Rust API with a string
+// or a Rust function to catch a JavaScript exception
+use wasm_bindgen::prelude::*;
+
+// build command for a wasm package that specifies web use
+// wasm-pack build --target web
+
+// callig the wasm_bindgen attribute that modifys the code that follows
+#[wasm_bindgen]
+// extern tells Rust that we want to call externally defined functions
+extern {
+    // this is a Rust function but it's also the alert function provided by JavaScript
+    pub fn alert(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+// producing a Rust functions that JavaScript can call
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
