2a2f17b64978874312e3bb99bd26d49a9a94334c
mindeleven <mindeleven@posteo.de>
2024-05-30 13:48:15 +0200

    Quickstart example with one endpoint

--- building-web-apis-with-rust/rocket-app-beginners/src/main.rs
+++ building-web-apis-with-rust/rocket-app-beginners/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+/// Rocket documentation: https://rocket.rs/guide/v0.5/
+/// Getting Started: https://rocket.rs/guide/v0.5/getting-started/#getting-started
+
+#[macro_use] extern crate rocket;
+
+// endpoint
+// can be called on terminal with `curl 127.0.0.1:8000``
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world from Rocket!\n"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
