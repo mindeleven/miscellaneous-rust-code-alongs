dd5156d25affc8b4a8e025e1d3c88262735b3213
mindeleven <mindeleven@posteo.de>
2024-06-03 13:51:36 +0200

    Added skip_deserializing for fields that should not be updated

--- building-web-apis-with-rust/rocket-app-beginners/src/models.rs
+++ building-web-apis-with-rust/rocket-app-beginners/src/models.rs
@@ -4,9 +4,11 @@ use crate::schema::rustaceans;
 
 #[derive(Serialize, Deserialize, Queryable, AsChangeset)]
 pub struct Rustacean {
+    #[serde(skip_deserializing)]
     pub id: i32,
     pub name: String,
     pub email: String,
+    #[serde(skip_deserializing)]
     pub created_at: String
 }
 
