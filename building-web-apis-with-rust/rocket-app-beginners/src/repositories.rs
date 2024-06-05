8e25abc08494699e283bcbd9bbe158b300137eca
mindeleven <mindeleven@posteo.de>
2024-06-05 12:13:02 +0200

    Implementing find method for Rustacean repository

--- building-web-apis-with-rust/rocket-app-beginners/src/repositories.rs
+++ building-web-apis-with-rust/rocket-app-beginners/src/repositories.rs
@@ -2,3 +2,10 @@ use crate::{models::Rustacean, schema::rustaceans};
 use diesel::prelude::*;
 pub struct RustaceanRepository;
 
+impl RustaceanRepository {
+    pub fn find(c: &mut SqliteConnection, id: i32) -> QueryResult<Rustacean> {
+        rustaceans::table
+            .find(id)
+            .get_result::<Rustacean>(c)
+    }
+}
