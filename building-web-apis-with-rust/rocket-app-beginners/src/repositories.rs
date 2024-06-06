1958ebeb891fab6008f2ead5cabd6f3265739f97
mindeleven <mindeleven@posteo.de>
2024-06-06 10:33:48 +0200

    Repo functionality to retrieve list of Rustacean from the database

--- building-web-apis-with-rust/rocket-app-beginners/src/repositories.rs
+++ building-web-apis-with-rust/rocket-app-beginners/src/repositories.rs
@@ -1,4 +1,7 @@
-use crate::{models::Rustacean, schema::rustaceans};
+use crate::{
+    models::Rustacean, 
+    schema::rustaceans
+};
 use diesel::prelude::*;
 pub struct RustaceanRepository;
 
@@ -7,5 +10,12 @@ impl RustaceanRepository {
         rustaceans::table
             .find(id)
             .get_result::<Rustacean>(c)
+    }
+
+    pub fn find_multiple(c: &mut SqliteConnection, limit: i64) -> QueryResult<Vec<Rustacean>> {
+        rustaceans::table
+            .order(rustaceans::id.desc())
+            .limit(limit)
+            .load::<Rustacean>(c)
     }
 }
