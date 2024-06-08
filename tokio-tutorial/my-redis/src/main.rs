bad50f128400f8b957adc93567d977d3a9b7c3a2
mindeleven <mindeleven@posteo.de>
2024-06-08 11:16:34 +0200

    Proceeding with the hello tokyo example and adding a few explanatory comments

--- tokio-tutorial/my-redis/src/main.rs
+++ tokio-tutorial/my-redis/src/main.rs
@@ -1,16 +1,33 @@
-use mini_redis::{client, Result};
+use mini_redis::{
+    client, 
+    Result
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // opening a connection to the mini_redis address
+    // the client::connect function is provided by the mini-redis crate
+    // connection returns client handle
     let mut client = client::connect("127.0.0.1:6379").await?;
+
+    let op = say_world();
     
+    // proceeding with the client handle
     // setting the key "hello" with value "world"
     client.set("hello", "world".into()).await?;
-
+    
+    // calls to .await within the async fn yield control back to the thread
     let result = client.get("hello").await?;
 
     println!("got value from the server; result={:?}", result);
 
+    println!("hello");
+
+    op.await;
+
     Ok(())
+}
+
+async fn say_world() {
+    println!("world");
 }
