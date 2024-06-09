2da131b3a60e4207ca742ddf6ab88a0ea9cee9bb
mindeleven <mindeleven@posteo.de>
2024-06-09 12:21:34 +0200

    Some code modifications for the spawning example

---
+++ tokio-tutorial/my-redis/examples/hello-redis.rs
@@ -0,0 +1,23 @@
+use mini_redis::{
+    client, 
+    Result
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // opening a connection to the mini_redis address
+    // the client::connect function is provided by the mini-redis crate
+    // connection returns client handle
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    
+    // proceeding with the client handle
+    // setting the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+    
+    // calls to .await within the async fn yield control back to the thread
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
+}
