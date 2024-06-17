cd299967d4330bb3090d46de97d13174c466952d
mindeleven <mindeleven@posteo.de>
2024-06-17 13:37:56 +0200

    Some explanatory comments

--- chat_server_with_rust_and_tokio/async_client_server/src/bin/client.rs
+++ chat_server_with_rust_and_tokio/async_client_server/src/bin/client.rs
@@ -1,3 +1,4 @@
+// single file executable for the chat client
 fn main() {
     unimplemented!()
 }
--- chat_server_with_rust_and_tokio/async_client_server/src/bin/server/main.rs
+++ chat_server_with_rust_and_tokio/async_client_server/src/bin/server/main.rs
@@ -1,3 +1,4 @@
+// src/bin/server is the server executable spread over four files
 fn main() {
     unimplemented!()
 }
