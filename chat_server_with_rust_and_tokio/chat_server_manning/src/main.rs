32609cdd81162be26418ab30bcf68ac390a50cbe
mindeleven <mindeleven@posteo.de>
2024-06-17 12:29:58 +0200

    Added tokio as a dependency and turned main into an async function

--- chat_server_with_rust_and_tokio/chat_server_manning/src/main.rs
+++ chat_server_with_rust_and_tokio/chat_server_manning/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+/// Creating a Chat Server with async Rust and Tokio by Lily Mara
+/// https://www.youtube.com/watch?v=T2mWg91sx-o
+/// tokio is asynchronous and non blocking, allows to write high performance network applications
+/// example project: chat server
+/// -> allowing multiple tcp clients to connect to the chat server
+/// -> when one client sends a message to the server, the message will be broadcast 
+///    to all the different clients
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
