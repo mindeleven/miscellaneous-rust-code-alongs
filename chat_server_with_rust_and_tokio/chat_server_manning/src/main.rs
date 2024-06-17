f30328c3eeb73b5e1bd2b879b53030b593e30677
mindeleven <mindeleven@posteo.de>
2024-06-17 12:50:54 +0200

    Some explanatory comments and adding a TcpListener to main

--- chat_server_with_rust_and_tokio/chat_server_manning/src/main.rs
+++ chat_server_with_rust_and_tokio/chat_server_manning/src/main.rs
@@ -5,8 +5,20 @@
 /// -> allowing multiple tcp clients to connect to the chat server
 /// -> when one client sends a message to the server, the message will be broadcast 
 ///    to all the different clients
+/// (Step 1) Making a TCP echo server
+///          -> server that waits for a client to connect
+///          -> once a client connects it will take any message the client sends 
+///             and bounce it right back to the client
+
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    // first thing we need is a TCP listener
+    // TcpListener is a type that comes from tokio
+    let listener = TcpListener::bind("localhost:8080")
+        .await // suspend current task until future is ready to be acted on
+        .unwrap();
+
     println!("Hello, world!");
 }
