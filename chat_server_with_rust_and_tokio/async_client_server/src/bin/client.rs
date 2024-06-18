dee5d79ebbfac112d06d067c78b0e36f4d45400d
mindeleven <mindeleven@posteo.de>
2024-06-18 10:24:02 +0200

    Elaborating on the chat client

--- chat_server_with_rust_and_tokio/async_client_server/src/bin/client.rs
+++ chat_server_with_rust_and_tokio/async_client_server/src/bin/client.rs
@@ -9,5 +9,26 @@ use async_std::net;
 
 async fn send_commands() -> ChatResult<()> {
     
+    println!("Commands:\n\
+              join GROUP\n\
+              post GROUP MESSAGE...\n\
+              Type Control-D (on Unix) or Control-Z (on Windows)\n\
+              to close the connection.");
+    
+    let mut command_lines = 
+        io::BufReader::new(io::stdin()).lines;
+
+    while let Some(commad_result) = command_lines.next().await {
+        let command = commad_result?;
+        // see github repo for definition of `parse_command``
+        let request = match parse_command(&command) {
+            Some(request) => result,
+            None => continue,            
+        };
+
+        utils::send_as_json(&mut to_server, &request).await?;
+        to_server.flush().await?;
+    }
+
     Ok(())
 }
