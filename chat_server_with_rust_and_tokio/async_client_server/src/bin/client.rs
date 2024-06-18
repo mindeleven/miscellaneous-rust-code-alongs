d978fa58e17115894b5b5b588411c5d8c894c4fd
mindeleven <mindeleven@posteo.de>
2024-06-18 10:08:50 +0200

    Scaffolding chat client

--- chat_server_with_rust_and_tokio/async_client_server/src/bin/client.rs
+++ chat_server_with_rust_and_tokio/async_client_server/src/bin/client.rs
@@ -1,4 +1,13 @@
-// single file executable for the chat client
-fn main() {
-    unimplemented!()
+/// single file executable for the chat client
+/// chat client's first responsibility: reading commands from the user
+/// in this example: reading lines from standard input
+
+use async_std::prelude::*;
+use async_chat::utils::{self, ChatResult};
+use async_std::io;
+use async_std::net;
+
+async fn send_commands() -> ChatResult<()> {
+    
+    Ok(())
 }
