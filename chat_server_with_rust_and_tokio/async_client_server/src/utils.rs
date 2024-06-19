64524216bdb7397827ab34e410b518af2993687c
mindeleven <mindeleven@posteo.de>
2024-06-19 09:54:37 +0200

    Copied send_as_json() and receive_as_json() from the original repo; at this point of the explanation not described in the book; explanatory comments needed asap

--- chat_server_with_rust_and_tokio/async_client_server/src/utils.rs
+++ chat_server_with_rust_and_tokio/async_client_server/src/utils.rs
@@ -1,5 +1,32 @@
 use std::error::Error;
+use serde::Serialize;
+use async_std::prelude::*;
 
 // result and error types we'll use throughout the application
 pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
 pub type ChatResult<T> = Result<T, ChatError>;
+
+pub async fn send_as_json<S, P>(outbound: &mut S, packet: &P) -> ChatResult<()>
+where
+    S: async_std::io::Write + Unpin,
+    P: Serialize,
+{
+    let mut json = serde_json::to_string(&packet)?;
+    json.push('\n');
+    outbound.write_all(json.as_bytes()).await?;
+    Ok(())
+}
+
+use serde::de::DeserializeOwned;
+
+pub fn receive_as_json<S, P>(inbound: S) -> impl Stream<Item = ChatResult<P>>
+    where S: async_std::io::BufRead + Unpin,
+          P: DeserializeOwned,
+{
+    inbound.lines()
+        .map(|line_result| -> ChatResult<P> {
+            let line = line_result?;
+            let parsed = serde_json::from_str::<P>(&line)?;
+            Ok(parsed)
+        })
+}
